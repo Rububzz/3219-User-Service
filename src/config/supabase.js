a5eea6fe9fde1c6077a17f4638d8796f3c082c6e
rubinnn <e1155263@u.nus.edu>
2026-02-26 17:38:14 +0800

    create empty function if for testing so supabase is not invoked

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 const config = require('./env');
 
-const supabase = createClient(config.supabaseUrl, config.supabaseServiceKey);
+const supabase = config.isTest
+    ? {}
+    : createClient(config.supabaseUrl, config.supabaseServiceKey);
 
 module.exports = supabase;
